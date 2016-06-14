0a6665d4144daade888f1e8cc7e16aff37492181
jorgeperis <jorgeperis@gmail.com>
2016-06-14 18:14:48 +0200

    Add blog with color, loop pages, data

--- Week 1/Day 2/blog.rb
+++ Week 1/Day 2/blog.rb
@@ -1,33 +1,47 @@
+require 'colorize'
+require 'date'
 class Blog
   def initialize(name)
     @name = name
-    @content = []
+    @post_hash ={}
+    @content =[]
   end
 
   def add_post(post)
     @content.push(post)
   end
 
-  def publish_post(publish)
-    publish.each do |x|
-      if x.sponsored
-        spon = "******#{x.title}******    #{x.data}"
+  def publish_post(page)
+
+    puts "\n\n"
+    @post_hash[page].each do |x|
+      x.view
+    end
+
+    pages.each do |x|
+      if x == page
+        print "#{x} ".colorize(:blue)
       else
-        spon = "#{x.title}                #{x.data}"
+        print "#{x} "
       end
-
-      puts spon
-      puts "***************"
-      puts x.text
-      puts "---------------\n\n\n"
     end
-
-    i=1
-    while i<=pages do
-      print "#{i}  "
-      i=i+1
+    puts "\n\n"
+    puts "<- Prev       Next ->"
+    
+    ans = gets.chomp
+    if ans.upcase == "NEXT"
+      if @post_hash[page+1]
+        publish_post(page+1)
+      else
+        publish_post(1)
+      end
+    elsif ans.upcase == "PREV"
+      if @post_hash[page-1]
+        publish_post(page-1)
+      else
+        publish_post(5)
+      end
     end
-    puts "\n\n\n"
   end
 
   def pages
@@ -37,48 +51,65 @@ class Blog
     else
       page = @content.length/3 + 1
     end
-    return page
+    i=1
+    page_array=[]
+    while i<=page do
+      page_array.push(i)
+      i=i+1
+    end
+    return page_array
   end
 
-  def pagination(start)
+  def pagination
     publish_page=[]
     publish = @content.sort_by {|item| item.data}.reverse
-    publish.each_with_index do |item,i|
-      if i == start
-        publish_page.push(publish[i])
-        publish_page.push(publish[i+1])
-        publish_page.push(publish[i+2])
-        @last = i+2
-      end
+    post_slice = publish.each_slice(3).to_a
+    pages.each_with_index do |item,i|
+    @post_hash[item] = post_slice[i]
     end
-    publish_post(publish_page)
   end
 end
 
 class Post
-  attr_reader :title,:data,:text,:sponsored
-  def initialize(title,data,text,sponsored=false)
+  attr_reader :data
+  def initialize(title,data,text)
     @title = title
     @data = data
     @text = text
-    @sponsored = sponsored
+  end
+
+  def view
+    puts "#{@title}                #{@data}"
+    puts "***************"
+    puts @text
+    puts "---------------\n\n\n"
+  end
+end
+
+class Sponsored < Post
+  def view
+    puts "******#{@title}******    #{@data.to_s}"
+    puts "***************"
+    puts @text
+    puts "---------------\n\n\n"
   end
 end
 
 blog = Blog.new("MyBlog")
-blog.add_post Post.new("Title1","20160614","Text1")
-blog.add_post Post.new("Title2","20160601","Text2")
-blog.add_post Post.new("Title3","20160602","Text3")
-blog.add_post Post.new("Title4","20160613","Text4",true)
-blog.add_post Post.new("Title5","20160603","Text5")
-blog.add_post Post.new("Title6","20160612","Text6")
-blog.add_post Post.new("Title7","20160604","Text7")
-blog.add_post Post.new("Title8","20160611","Text8",true)
-blog.add_post Post.new("Title9","20160605","Text9")
-blog.add_post Post.new("Title10","20160610","Text10")
-blog.add_post Post.new("Title11","20160606","Text11")
-blog.add_post Post.new("Title12","20160609","Text12",true)
-blog.add_post Post.new("Title13","20160607","Text13")
-blog.add_post Post.new("Title14","20160608","Text14")
+blog.add_post Post.new("Title1",Date.new(2016,06,14),"Text1")
+blog.add_post Post.new("Title2",Date.new(2016,06,01),"Text2")
+blog.add_post Sponsored.new("Title3",Date.new(2016,06,02),"Text3")
+blog.add_post Post.new("Title4",Date.new(2016,06,13),"Text4")
+blog.add_post Post.new("Title5",Date.new(2016,06,03),"Text5")
+blog.add_post Sponsored.new("Title6",Date.new(2016,06,12),"Text6")
+blog.add_post Sponsored.new("Title7",Date.new(2016,06,04),"Text7")
+blog.add_post Post.new("Title8",Date.new(2016,06,11),"Text8")
+blog.add_post Post.new("Title9",Date.new(2016,06,05),"Text9")
+blog.add_post Sponsored.new("Title10",Date.new(2016,06,10),"Text10")
+blog.add_post Post.new("Title11",Date.new(2016,06,06),"Text11")
+blog.add_post Post.new("Title12",Date.new(2016,6,9),"Text12")
+blog.add_post Post.new("Title13",Date.new(2016,6,7),"Text13")
+blog.add_post Post.new("Title14",Date.new(2016,6,8),"Text14")
 
-blog.pagination(0)
+blog.pagination
+blog.publish_post(1)
