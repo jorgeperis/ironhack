824875c1dfad8d5c290fefd70f57b0fdee3f1025
jorgeperis <jorgeperis@gmail.com>
2016-06-18 09:55:07 +0200

    Add chess with pawn working

--- Week 1/Day 5/chess.rb
+++ Week 1/Day 5/chess.rb
@@ -2,17 +2,135 @@ require 'pry'
 
 class Tokens
 
-  def initialize(movements,board)
-    @movements = movements
+  def initialize(board,start,finish)
     @board = board
+    @start = start
+    @finish = finish
+    @x_start = @start[0]
+    @y_start = @start[1]
+
+  end
+end
+
+
+
+class Pawn < Tokens
+
+  def validator
+
+    token = @board[@start]
+
+    if token.include? 'b'
+      b_validator(token)
+    else
+      w_validator(token)
+    end
+
+  end
+
+
+  def w_validator(token)
+    my_color = 'w'
+    opp_color = 'b'
+
+
+    if @start[1] == 2 #one or two movements
+      if @board[@finish] == "--"
+        if @finish == [@x_start,@y_start + 1]
+          return  puts "LEGAL"
+        elsif @finish == [@x_start,@y_start + 2] && @board[[@x_start,@y_start + 1]] == "--"
+          return puts "LEGAL"
+        else
+          return puts "ILLEGAL"
+        end
+      elsif @board[@finish].include? 'b'
+        if @finish == [@x_start - 1,@y_start + 1] || @finish == [@x_start + 1,@y_start + 1]
+          return puts "LEGAL"
+        else
+          return puts "ILLEGAL"
+        end
+      else
+        return puts "ILLEGAL"
+      end
+    else #one movement
+      if @board[@finish] == '--' && @finish = [@x_start,@y_start+1]
+        return puts "LEGAL"
+      else
+        return puts "ILLEGAL"
+      end
+    end
+
+  end
+
+  def b_validator(token)
+    my_color = 'b'
+    opp_color = 'w'
+
+
+
+    if @start[1] == 7 #one or two movements
+      if @board[@finish] == "--"
+        if @finish == [@x_start,@y_start -1]
+          return puts "LEGAL"
+        elsif @finish == [@x_start,@y_start - 2] && @board[[@x_start,@y_start - 1]] == "--"
+          return puts "LEGAL"
+        else
+          return puts "ILLEGAL"
+        end
+      elsif @board[@finish].include? 'w'
+        if @finish == [@x_start - 1,@y_start - 1] || @finish == [@x_start + 1,@y_start - 1]
+          return puts "LEGAL"
+        else
+          return puts "ILLEGAL"
+        end
+      else
+        return puts "ILLEGAL"
+      end
+    else #one movements
+
+      if @board[@finish] == '--' && @finish = [@x_start,@y_start-1]
+        return puts "LEGAL"
+      end
+    end
+  end
+
+
+end
+
+
+class Knight < Tokens
+
+  def validator
+    puts "knight"
+  end
+
+end
+
+class Board
+
+  def initialize(board,movements)
+    @board = board
+    @movements = movements
+    @start = ""
+    @finish =""
+  end
+
+  def loopMovements
+
+    @movements.each do |x|
+      @start = x[0]
+      @finish = x[1]
+      tokenSort(@board[@start])
+    end
+
   end
 
   def tokenSort(token)
 
     if token.include? "P"
-      puts "pawn"
+      Pawn.new(@board,@start,@finish).validator
     elsif token.include? "N"
-      puts "knight"
+      Knight.new(@board,@start,@finish).validator
     elsif token.include? "B"
       puts "bishop"
     elsif token.include? "R"
@@ -21,18 +139,12 @@ class Tokens
       puts "queen"
     elsif token.include? "K"
       puts "king"
-    end
-
-  end
-
-  def loopMovements
-    @movements.each do |x|
-      tokenSort(@board[x[0].to_sym])
+    else
+      puts "ILLEGAL"
     end
   end
-
-
 end
+
 
 def getMovements_file(file)
 
@@ -47,24 +159,36 @@ def getMovements_file(file)
   return movements_array
 end
 
+def convert_movements_to_local_system(movements)
+  local_movements=[]
+  change = {"a" => 1,"b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8}
+
+  movements.each do |x|
+    local_movements.push([[change[x[0][0]],x[0][1].to_i],[change[x[1][0]],x[1][1].to_i]])
+  end
+
+return local_movements
+end
+
 def getBoard_file(file)
   hash_position={}
   board = IO.read(file).split(" ")
 
   i=0
   [8,7,6,5,4,3,2,1].each do |y|
-    ('a'..'h').each do |x|
-      hash_position["#{x}#{y}".to_sym] = board[i]
-      #print "#{board[i]} "
+    (1..8).each do |x|
+      hash_position[[x,y]] = board[i]
+      print "#{board[i]} "
       i += 1
     end
-    #puts "\n"
+    puts "\n"
   end
   return hash_position
 end
 
 movements = getMovements_file("simple_moves.txt")
+local_movements = convert_movements_to_local_system(movements)
 board = getBoard_file("simple_board.txt")
 
-tokens = Tokens.new(movements,board)
-tokens.loopMovements
+board = Board.new(board,local_movements)
+board.loopMovements
