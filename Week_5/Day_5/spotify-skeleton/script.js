62c49fd477701c1b1fb960a15a9a393a7a7db095
jorgeperis <jorgeperis@gmail.com>
2016-07-15 17:38:27 +0200

    Add refactor spotify player

--- Week_5/Day_5/spotify-skeleton/script.js
+++ Week_5/Day_5/spotify-skeleton/script.js
@@ -4,17 +4,24 @@ $(function () {
   var arrayPosition = 0;
   var myArtist;
 
-  $('.search-form').on('submit', function(e){
+  var SpotifySearch = function(e){
+    e.preventDefault();
+    searchView();
+    searchModel();
+  };
+
+  function searchView(){
     $('[name="more-results"]').prop('disabled',false);
     $('.btn-play').removeClass('playing');
     $('.btn-play').addClass('disabled');
-    e.preventDefault();
+  }
+  function searchModel(){
     var track = $('[name="track"]').val();
     $.ajax({
       url: 'https://api.spotify.com/v1/search?type=track&query=' + track,
       success: showPlayerResults
     })
-  })
+  }
 
   var showPlayerResults = function(result){
     myResult = result;
@@ -32,7 +39,7 @@ $(function () {
     $('.btn-play').removeClass('disabled');
   }
 
-  $('.btn-play').on('click', function(){
+  var SpotifyPlayer = function(){
     if ($(this).hasClass('playing')) {
       $('.js-player').trigger('pause');
       $('.btn-play').removeClass('playing');
@@ -40,79 +47,87 @@ $(function () {
       $('.js-player').trigger('play');
       $('.btn-play').addClass('playing');
     }
-  })
+  }
 
-function printTime () {
-  var current = $('.js-player').prop('currentTime');
-  $('progress').val(current);
-}
-$('.js-player').on('timeupdate', printTime);
+  var SpotifyArtist = function(){
+    $.ajax({
+      url: 'https://api.spotify.com/v1/artists/' + myTrackArtistId,
+      success: function(artist){
+        myArtist = artist;
+        $('.js-artist-name').text(artist.name);
+        $('.js-artist-image').attr('src',artist.images[1].url);
+        $('.js-artist-followers').text(artist.followers.total);
+        $('.js-artist-popularity').text(artist.popularity);
+        $('[name="see-albums"]').val("See " + artist.name + " albums");
+      }
+    })
+    $('.js-modal').modal("show");
+  }
 
+  var SpotifyMoreResults = function(){
+    $('.list-more-results').empty();
+    $('.js-track-name').text($('[name="track"]').val());
+    var ulMoreResults = $('.list-more-results');
+    myResult.tracks.items.forEach( function(track,index){
+      var liTrackName = $('<li>').text(track.name).addClass('more-results-track').attr('id',index);
+      ulMoreResults.append(liTrackName);
+    })
+    $('.js-more-results').modal('show');
+  }
 
-$('.author').on('click', function(){
-  $.ajax({
-    url: 'https://api.spotify.com/v1/artists/' + myTrackArtistId,
-    success: function(response){
-      myArtist = response;
-      var myArtistPopularity = response.popularity;
-      var myArtistFollowers = response.followers.total;
-      var myArtistImageScr = response.images[1].url;
-      var myArtistName = response.name;
+  var SpotifySelectedTrack = function(){
+    arrayPosition = parseInt($(this).attr('id'));
+    showPlayerResults(myResult);
+    $('.js-more-results').modal('hide');
+    $('progress').val("0");
+    $('.btn-play').removeClass('playing');
+  }
 
-      $('.js-artist-name').text(myArtistName);
-      $('.js-artist-image').attr('src',myArtistImageScr);
-      $('.js-artist-followers').text(myArtistFollowers);
-      $('.js-artist-popularity').text(myArtistPopularity);
-      $('[name="see-albums"]').val("See " + myArtistName + " albums")
-    }
-  })
-  $('.js-modal').modal("show");
-});
-$('[name="more-results"]').on('click', function(e){
-  $('.list-more-results').empty();
-  $('.js-track-name').text($('[name="track"]').val());
-  var ulMoreResults = $('.list-more-results');
-  myResult.tracks.items.forEach( function(track,index){
-    var liTrackName = $('<li>').text(track.name).addClass('more-results-track').attr('id',index);
-    ulMoreResults.append(liTrackName);
-  })
-  $('.js-more-results').modal('show');
-})
-$(document).on('click','.more-results-track', function(){
-  arrayPosition = parseInt($(this).attr('id'));
-  showPlayerResults(myResult);
-  $('.js-more-results').modal('hide');
-})
-$('[name="see-albums"]').on('click', function(){
-  $('.list-artist-albums').empty();
-  $('.js-artist-albums-name').text(myArtist.name);
-  $('.js-modal').modal('hide');
+  var SpotifySeeAlbums = function(){
+    $('.list-artist-albums').empty();
+    $('.js-artist-albums-name').text(myArtist.name);
+    $('.js-modal').modal('hide');
 
-  $.ajax({
-    url: 'https://api.spotify.com/v1/artists/' + myArtist.id + '/albums',
-    success: function(response){
-      var albumsArray = response.items;
-      albumsArray.forEach( function(album,i){
-        $.ajax({
-          url: 'https://api.spotify.com/v1/albums/' + album.id,
-          success: function(albumInfo){
-            var releasedDate = albumInfo.release_date;
-            var textRelease = $('<span>').text('Released: ').addClass('albumReleased')
-            var varRelease = $('<span>').text(releasedDate);
-            var paraTwo = $('<p>').append(textRelease).append(varRelease);
-            var textName = $('<span>').text('Name: ').addClass('albumName')
-            var varName = $('<span>').text(albumInfo.name);
-            var paraOne = $('<p>').append(textName).append(varName);
-            var coverAlbum = $('<img>').attr('src',albumInfo.images[2].url)
-            var li = $('<li>').append(paraOne).append(paraTwo).append(coverAlbum)
-            $('.list-artist-albums').append(li);
-            if (i == (albumsArray.length - 1)) {
-              $('.js-artist-albums').modal('show');
+    $.ajax({
+      url: 'https://api.spotify.com/v1/artists/' + myArtist.id + '/albums',
+      success: function(response){
+        var albumsArray = response.items;
+        albumsArray.forEach( function(album,i){
+          $.ajax({
+            url: 'https://api.spotify.com/v1/albums/' + album.id,
+            success: function(albumInfo){
+              var releasedDate = albumInfo.release_date;
+              var textRelease = $('<span>').text('Released: ').addClass('albumReleased')
+              var varRelease = $('<span>').text(releasedDate);
+              var paragraphTwo = $('<p>').append(textRelease).append(varRelease);
+              var textName = $('<span>').text('Name: ').addClass('albumName')
+              var varName = $('<span>').text(albumInfo.name);
+              var paragraphOne = $('<p>').append(textName).append(varName);
+              var coverAlbum = $('<img>').attr('src',albumInfo.images[2].url)
+              var li = $('<li>').append(paragraphOne).append(paragraphTwo).append(coverAlbum)
+              $('.list-artist-albums').append(li);
+              if (i == (albumsArray.length - 1)) {
+                $('.js-artist-albums').modal('show');
+              }
             }
-          }
+          })
         })
-      })
-    }
-  })
-})
+      }
+    })
+  }
+
+  function printTime () {
+    var current = $('.js-player').prop('currentTime');
+    $('progress').val(current);
+  }
+
+  $('.js-player').on('timeupdate', printTime);
+
+  $('.search-form').on('submit', SpotifySearch);
+  $('.btn-play').on('click', SpotifyPlayer);
+  $('.author').on('click', SpotifyArtist);
+  $('[name="more-results"]').on('click', SpotifyMoreResults);
+  $(document).on('click','.more-results-track', SpotifySelectedTrack);
+  $('[name="see-albums"]').on('click', SpotifySeeAlbums);
+
 })
